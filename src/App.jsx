c66bf167ce5314638321823418ea82ca71901116
Anant Verma <anant.verma0908@gmail.com>
2023-06-10 23:41:06 +0530

    Add app bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
+import AppBarComponent from './appBar/AppBar'
+import Home from './home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      test
+      <AppBarComponent>
+        <Home />
+      </AppBarComponent>
     </>
   )
 }
